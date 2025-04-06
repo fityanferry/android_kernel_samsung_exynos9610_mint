6e6b766802b8d7eaca17f0e9507e7ef3f5ef9de7
fityanferry <tthanzdantez98@gmail.com>
2025-04-06 10:48:03 +0000

    Add KernelSU-Next as submodule

--- build.sh
+++ build.sh
@@ -446,7 +446,7 @@ mkdir -p "$TMP_DIR"
 VERIFY_TOOLCHAIN
 VERIFY_DEFCONFIG
 
-git submodule update --init "$TOP/KernelSU"
+git submodule update --init "$TOP/KernelSU-Next"
 
 if $BUILD_KERNEL_CI; then
 	export KBUILD_BUILD_USER="Clembot"
